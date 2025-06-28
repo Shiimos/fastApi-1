f9b30d2eec5734364b1a677cb167800eda166765
Ismael Otmani <ismael2006otmani@gmail.com>
2025-06-28 20:56:49 +0100

    Path parameters learned !

--- myapi.py
+++ myapi.py
@@ -1,1 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
+
+app = FastAPI()
+
+students = {
+    1:{
+        "name": "Ismael",
+        "age": 19,
+        "classe": "Data structures"
+    }
+}
+
+@app.get("/")
+def index():
+    return {"name": "first data"}
+
+@app.get("/get-student/{student_id}")
+def get_student(student_id: int = Path(..., description="Must input the idea of the student", gt = 0, lt = 3)):
+    return students[student_id]
